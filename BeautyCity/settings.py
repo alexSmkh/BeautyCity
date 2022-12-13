0de0bc1a02288e66632311fc4b953533142cb011
alexSmkh <42808121+alexSmkh@users.noreply.github.com>
2022-12-13 22:38:01 +0300

    Revert "Create User app"

--- BeautyCity/settings.py
+++ BeautyCity/settings.py
@@ -31,9 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
-    'phonenumbers',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +115,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.User'
